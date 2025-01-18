31e041d08de84f6a88eb6e9020266b96b7a11104
adechotechnologies <152974710+adechotechnologies@users.noreply.github.com>
2025-01-18 04:02:12 -0800

    vector

---
+++ Lec7Vector.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    // vector<int> vec={1,2,3}; //3
+    // cout<<vec[0]; //1
+
+    // vector<int> vec(3,0);
+    // cout<<vec[0];//0
+    // cout<<vec[1];//0
+    // cout<<vec[2];//0
+
+
+    //  FOR EACH LOOP
+    // vector<int> vec(5,0);
+    // for(int i:vec){
+    //     cout<<i<<endl;
+    // }
+
+    // vector<char> vec={'a','b','c','d','e'};
+    // for(char i:vec){
+    //     cout<<i<<endl;
+    // }
+
+
+    //1.VECTOR FUNCTIONS
+    // vector<char> vec;
+    // cout<<"size="<<vec.size()<<endl;
+    // // add
+    // // value will be add at end
+    // vec.push_back('a');
+    // vec.push_back('b');
+    // vec.push_back('c');
+    // // del
+    // vec.pop_back(); //last value will delete
+    // cout<<"size="<<vec.size()<<endl;
+
+    // cout<<vec.front(); //a
+    // cout<<vec.back(); //c
+    // cout<<vec.at(1); //b
+
+    // 2.FOR EACH LOOP
+    // for(char i:vec){
+    //     cout<<i<<endl;
+    // }
+
+
+    //3.STATIC VS DYNAMIC ALLOCATION IN MEMORY
+    //staitc memory allocation done on compile time. it happen in stack
+    //dynamic allocation done in run time. it happen in heap memory
+    // For ex push-back function
+    
+
+    
+
+    return 0;
+}
